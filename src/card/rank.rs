60df95ddbc9e4c0938cdfdee9f5bad7ce6949976
Curtis McEnroe <programble@gmail.com>
2016-03-07 20:23:10 -0500

    Impl Into<char> for Rank

--- src/card/rank.rs
+++ src/card/rank.rs
@@ -28,3 +28,23 @@ impl Rank {
         }
     }
 }
+
+impl Into<char> for Rank {
+    fn into(self) -> char {
+        match self {
+            Rank::Ace => 'A',
+            Rank::Two => '2',
+            Rank::Three => '3',
+            Rank::Four => '4',
+            Rank::Five => '5',
+            Rank::Six => '6',
+            Rank::Seven => '7',
+            Rank::Eight => '8',
+            Rank::Nine => '9',
+            Rank::Ten => 'T',
+            Rank::Jack => 'J',
+            Rank::Queen => 'Q',
+            Rank::King => 'K',
+        }
+    }
+}
