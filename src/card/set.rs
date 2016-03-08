3642ba8f5022f26c9059d24e3cd123cf97f4a13a
Curtis McEnroe <programble@gmail.com>
2016-03-08 11:50:00 -0500

    Use Rank::succ for Set::next

--- src/card/set.rs
+++ src/card/set.rs
@@ -19,25 +19,21 @@ impl Iterator for Set {
     fn next(&mut self) -> Option<Card> {
         self.card = match self.card {
             None => Some(Card { suit: Suit::Club, rank: Rank::Ace }),
+
+            Some(Card { suit: Suit::Club, rank: Rank::King }) => {
+                Some(Card { suit: Suit::Diamond, rank: Rank::Ace })
+            },
+            Some(Card { suit: Suit::Diamond, rank: Rank::King }) => {
+                Some(Card { suit: Suit::Heart, rank: Rank::Ace })
+            },
+            Some(Card { suit: Suit::Heart, rank: Rank::King }) => {
+                Some(Card { suit: Suit::Spade, rank: Rank::Ace })
+            },
             Some(Card { suit: Suit::Spade, rank: Rank::King }) => None,
-            Some(card) => Some(match card {
-                Card { suit, rank: Rank::Ace } => Card { suit: suit, rank: Rank::Two },
-                Card { suit, rank: Rank::Two } => Card { suit: suit, rank: Rank::Three },
-                Card { suit, rank: Rank::Three } => Card { suit: suit, rank: Rank::Four },
-                Card { suit, rank: Rank::Four } => Card { suit: suit, rank: Rank::Five },
-                Card { suit, rank: Rank::Five } => Card { suit: suit, rank: Rank::Six },
-                Card { suit, rank: Rank::Six } => Card { suit: suit, rank: Rank::Seven },
-                Card { suit, rank: Rank::Seven } => Card { suit: suit, rank: Rank::Eight },
-                Card { suit, rank: Rank::Eight } => Card { suit: suit, rank: Rank::Nine },
-                Card { suit, rank: Rank::Nine } => Card { suit: suit, rank: Rank::Ten },
-                Card { suit, rank: Rank::Ten } => Card { suit: suit, rank: Rank::Jack },
-                Card { suit, rank: Rank::Jack } => Card { suit: suit, rank: Rank::Queen },
-                Card { suit, rank: Rank::Queen } => Card { suit: suit, rank: Rank::King },
-                Card { suit: Suit::Club, .. } => Card { suit: Suit::Diamond, rank: Rank::Ace },
-                Card { suit: Suit::Diamond, .. } => Card { suit: Suit::Heart, rank: Rank::Ace },
-                Card { suit: Suit::Heart, .. } => Card { suit: Suit::Spade, rank: Rank::Ace },
-                _ => unreachable!(),
-            }),
+
+            Some(Card { suit, rank }) => {
+                Some(Card { suit: suit, rank: rank.succ().unwrap() })
+            },
         };
         self.card
     }
