071f87942a78cafcc9ade223b2f3f0f91e77f0d6
Curtis McEnroe <programble@gmail.com>
2016-03-10 14:45:48 -0500

    Unit test Play::Draw, Play::Redeal, Play::WasteTableau

--- src/game/klondike.rs
+++ src/game/klondike.rs
@@ -169,3 +169,94 @@ impl Klondike {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use card::{Suit, Rank, Card, Face, Pile};
+    use game::Game;
+    use super::{Klondike, Draw, Play, Foundation, Tableau};
+
+    #[test]
+    fn valid_draw_full_stock() {
+        let game = Klondike::new(Draw::One);
+        assert!(game.is_valid(&Play::Draw));
+    }
+
+    #[test]
+    fn invalid_draw_empty_stock() {
+        let mut game = Klondike::new(Draw::One);
+        game.stock = Pile::new();
+        assert!(!game.is_valid(&Play::Draw));
+    }
+
+    #[test]
+    fn valid_redeal_empty_stock() {
+        let mut game = Klondike::new(Draw::One);
+        game.stock.deal_to(&mut game.waste, 52, true);
+        assert!(game.is_valid(&Play::Redeal));
+    }
+
+    #[test]
+    fn invalid_redeal_empty_waste() {
+        let game = Klondike::new(Draw::One);
+        assert!(!game.is_valid(&Play::Redeal));
+    }
+
+    #[test]
+    fn invalid_redeal_non_empty_stock() {
+        let mut game = Klondike::new(Draw::One);
+        game.stock.deal_to(&mut game.waste, 1, true);
+        assert!(!game.is_valid(&Play::Redeal));
+    }
+
+    #[test]
+    fn invalid_redeal_empty_stock_empty_waste() {
+        let mut game = Klondike::new(Draw::One);
+        game.stock = Pile::new();
+        assert!(!game.is_valid(&Play::Redeal));
+    }
+
+    #[test]
+    fn valid_waste_tableau_king() {
+        let mut game = Klondike::new(Draw::One);
+        game.waste.push(Face::Up(Card::new(Suit::Club, Rank::King)));
+        assert!(game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+
+    #[test]
+    fn valid_waste_tableau_pred_color() {
+        let mut game = Klondike::new(Draw::One);
+        game.waste.push(Face::Up(Card::new(Suit::Heart, Rank::Queen)));
+        game.tableau[0].push(Face::Up(Card::new(Suit::Club, Rank::King)));
+        assert!(game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+
+    #[test]
+    fn invalid_waste_tableau_empty_waste() {
+        let game = Klondike::new(Draw::One);
+        assert!(!game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+
+    #[test]
+    fn invalid_waste_tableau_non_king() {
+        let mut game = Klondike::new(Draw::One);
+        game.waste.push(Face::Up(Card::new(Suit::Heart, Rank::Queen)));
+        assert!(!game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+
+    #[test]
+    fn invalid_waste_tableau_non_color() {
+        let mut game = Klondike::new(Draw::One);
+        game.waste.push(Face::Up(Card::new(Suit::Heart, Rank::Queen)));
+        game.tableau[0].push(Face::Up(Card::new(Suit::Diamond, Rank::King)));
+        assert!(!game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+
+    #[test]
+    fn invalid_waste_tableau_non_pred() {
+        let mut game = Klondike::new(Draw::One);
+        game.waste.push(Face::Up(Card::new(Suit::Heart, Rank::Jack)));
+        game.tableau[0].push(Face::Up(Card::new(Suit::Club, Rank::King)));
+        assert!(!game.is_valid(&Play::WasteTableau(Tableau::One)));
+    }
+}
