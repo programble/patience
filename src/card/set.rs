25ecea9ceda4e848b86399e81031305172425fe8
Curtis McEnroe <programble@gmail.com>
2016-03-10 20:28:02 -0500

    Mirror rank order from Set
    This is the order a new pack of Bicycle cards come in.

--- src/card/set.rs
+++ src/card/set.rs
@@ -21,13 +21,14 @@ impl Iterator for Set {
             None => Some(card!(H A)),
 
             Some(card!(H K)) => Some(card!(C A)),
-            Some(card!(C K)) => Some(card!(D A)),
-            Some(card!(D K)) => Some(card!(S A)),
-            Some(card!(S K)) => None,
+            Some(card!(C K)) => Some(card!(D K)),
+            Some(card!(D A)) => Some(card!(S K)),
+            Some(card!(S A)) => None,
 
-            Some(Card { suit, rank }) => {
-                Some(Card { suit: suit, rank: rank.succ().unwrap() })
-            },
+            Some(Card { suit: suit!(H), rank }) => Some(Card::new(suit!(H), rank.succ().unwrap())),
+            Some(Card { suit: suit!(C), rank }) => Some(Card::new(suit!(C), rank.succ().unwrap())),
+            Some(Card { suit: suit!(D), rank }) => Some(Card::new(suit!(D), rank.pred().unwrap())),
+            Some(Card { suit: suit!(S), rank }) => Some(Card::new(suit!(S), rank.pred().unwrap())),
         };
         self.card
     }
